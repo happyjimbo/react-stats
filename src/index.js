f9a303d036ec18bdc5d4b88f07c0c330c5b8dfd5
James Woodward <james.woodward@spaceapegames.com>
2016-12-30 10:36:42 +0000

    Added persistent storage of the state, so stocks that have been added are cached

--- src/index.js
+++ src/index.js
@@ -1,24 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware/*, compose*/ } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunkMiddleware from 'redux-thunk';
 import App from './App';
 import './css/index.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import helloWorldApp from './reducers/helloWorldApp';
+import CombineReducers from './reducers/CombineReducers';
 import createLogger from 'redux-logger';
+import {persistStore, autoRehydrate} from 'redux-persist'
 
-
-//import logger from 'redux-logger';
-
-//const winstonLogger = new (winston.Logger)();
 const logger = createLogger();
-const store = createStore(helloWorldApp, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(), 
-	applyMiddleware(thunkMiddleware, logger)
-);
-//const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-//const store = createStore(helloWorldApp, composeEnhancers(applyMiddleware(logger)) );
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(CombineReducers, undefined, composeEnhancers(
+  applyMiddleware(thunkMiddleware, logger),
+  autoRehydrate()
+));
+
+persistStore(store);
 
 ReactDOM.render(
   <Provider store={store}>
