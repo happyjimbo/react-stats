d3086a2f849abb3d494e0d1f75efe8cc67bae853
James Woodward <james.woodward@spaceapegames.com>
2017-01-04 13:40:58 +0000

    deleted unused dialog state and also added the start of the stats action tests

---
+++ src/test/actions/StatsAction.test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+import StatsAction from '../../actions/StatsAction';
+import renderer from 'react-test-renderer';
+
+// http://redux.js.org/docs/recipes/WritingTests.html
+describe('StatsAction', () => {
+
+    it("test", () => {
+
+    });
+
+});
