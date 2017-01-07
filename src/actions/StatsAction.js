4a402ef8d0c5c9056634ba4ec119229c58621cb4
James Woodward <james.woodward@spaceapegames.com>
2017-01-07 11:22:23 +0000

    Added test for StatsAction, testing that the correct action is triggered when fetchStat is called

--- src/actions/StatsAction.js
+++ src/actions/StatsAction.js
@@ -1,6 +1,7 @@
 import {RECEIVE_STAT, RECEIVE_STAT_FAIL} from '../consts/StatsConsts';
 import fetch from 'isomorphic-fetch';
 
+// Look at doing this wthout passing in dispatch.
 export function storedStats(state, dispatch) {
 
     return (dispatch) => { 
@@ -24,7 +25,6 @@ export function fetchStat(stat) {
 
 function validateResponse(stat, data) {
 
-
     if (data.query !== undefined) {
         let name = data.query.results.quote.Name;
         if (name !== null) {
@@ -39,8 +39,7 @@ export function receieveStat(stat, data) {
     return {
         type: RECEIVE_STAT,
         stat,
-        data,
-        receivedAt: Date.now()
+        data
     }
 }
 
@@ -48,11 +47,10 @@ export function receieveStatFail(stat, message) {
     return {
         type: RECEIVE_STAT_FAIL,
         stat,
-        message: message,
-        receivedAt: Date.now()
+        message: message
     }
 }
 
-function getStatUrl(property) {
+export function getStatUrl(property) {
     return `https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quote%20where%20symbol%20in%20(%22${property}%22)&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=`;
 }
--- src/test/actions/StatsAction.test.js
+++ src/test/actions/StatsAction.test.js
@@ -1,13 +1,53 @@
-import React from 'react';
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
 import {shallow} from 'enzyme';
-import StatsAction from '../../actions/StatsAction';
+import {getStatUrl, fetchStat} from '../../actions/StatsAction';
 import renderer from 'react-test-renderer';
+import nock from 'nock';
+import {RECEIVE_STAT} from '../../consts/StatsConsts';
 
-// http://redux.js.org/docs/recipes/WritingTests.html
+const middlewares = [ thunk ];
+const mockstore = configureMockStore(middlewares);
+
+
 describe('StatsAction', () => {
+
+    afterEach(() => {
+        nock.cleanAll();
+    });
 
     it("test", () => {
 
+        const testObject = {
+            query: {
+                results: {
+                    quote: {
+                        Name: "test"
+                    }                    
+                }
+            }
+        };
+
+        const stat = 'RBS.L';
+        const url = 'https://query.yahooapis.com';
+
+        nock(url)
+        .filteringPath(path => '/')
+        .get('/')
+        .reply(200, testObject);
+
+        const expectedActions = [{ 
+            type: RECEIVE_STAT, 
+            stat,
+            data: testObject 
+        }];
+
+        const store = mockstore({});
+
+        return store.dispatch(fetchStat(stat))
+        .then(() => { 
+            expect(store.getActions()).toEqual(expectedActions);
+        });
     });
 
 });
